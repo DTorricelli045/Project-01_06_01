6253d29b3eeb043306e8213e09bac58ab769f69c
Unknown <DTorricelli045@west-mec.org>
2018-08-16 15:26:09 -0700

    Fixed Load error
    Repositioned files to correct position to load the page javascript properly

---
+++ script.js
@@ -0,0 +1,35 @@
+/*  Project 01_06_01
+
+    Peronal Information Form Validation Code
+      Author: Dominic Torricelli
+      Date:   16 August 2018
+
+    Filename: script.js
+*/
+
+"use strict";
+
+//Global variables
+var formValidity = true;
+
+
+
+
+
+
+//Function to run function on submit
+function validateForm() {
+//Check line 189 of snoot.js for previous code
+}
+
+//Function to run a function when the page is loaded
+function runCode() {
+alert("RUN")
+}
+
+//Page load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", runCode, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", runCode);
+}
