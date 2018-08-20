780662377355e4200ddb85aac48290938837edce
Unknown <DTorricelli045@west-mec.org>
2018-08-20 13:49:59 -0700

    Furthest attempt
    Everything is broken, so I am going to restart with an earlier file

--- script.js
+++ script.js
@@ -13,23 +13,74 @@
 var formValidity = true;
 
 
+function validateRequired(fieldsetId) {
+    var inputElements = document.querySelectorAll("#contactinfo input");
+    var fieldsetValidity = true;
+    var currentElement;
+
+    // look for invalid inputs
+    try {
+
+        for (var i = 0; i < inputElements.length; i++) {
+            currentElement = inputElements[i];
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255,233,233)";
+                fieldsetValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+    } catch (err) {
+
+    }
 
 
 
+    // function to validate form on submit button input
+    function validateForm(ret) {
+        // prevent default submit behavior
+        if (ret.preventDefault) {
+            ret.preventDefault();
+        } else {
+            ret.returnValue = false;
+        }
+        formValidity = true;
 
-//Function to run function on submit
-function validateForm() {
-//Check line 189 of snoot.js for previous code
-}
+        validateRequired(); // Located on line 16
 
-//Function to run a function when the page is loaded
-function runCode() {
-alert("RUN")
-}
+        if (formValidity === true) { // form is valid
+            document.getElementById("errorText").innerHTML = "";
+            document.getElementById("errorText").style.display = "none";
+            document.getElementsByTagName("form")[0].submit();
+        } else {
+            document.getElementById("errorText").innerHTML = "Please fix the required fields.";
+            document.getElementById("errorText").style.display = "block";
+            scroll(0, 0);
+        }
+    }
 
-//Page load event handlers
-if (window.addEventListener) {
-    window.addEventListener("load", runCode, false);
-} else if (window.attachEvent) {
-    window.attachEvent("onload", runCode);
+    //Function to run code on page load
+    function runCode() {
+        createEventListeners();
+    }
+
+    //Function to Run various Event listeners
+    function createEventListeners() {
+
+        //Variable to call on the submit button
+        var form = document.getElementsByTagName("form")[0];
+        if (form.addEventListener) {
+            form.addEventListener("submit", validateForm, false);
+        } else if (form.attachEvent) {
+            form.attachEvent("onsubmit", validateForm);
+        }
+    }
+
+
+    //Page load event handlers
+    if (window.addEventListener) {
+        window.addEventListener("load", runCode, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onload", runCode);
+    }
 }
